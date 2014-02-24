bff674e971bef834ea64c7ea7b53730c1535892e
aaronwolen <aaron@wolen.com>
2014-02-24 12:57:44 -0500

    Add installation.r

---
+++ 1-installation.r
@@ -0,0 +1,19 @@
+# bingd tutorial
+
+install.packages(c("devtools", "plyr"))
+library(devtools)
+
+bioc.deps <- c("GenomicRanges", "AnnotationHub")
+
+source("http://www.bioconductor.org/biocLite.R")
+biocLite(bioc.deps, suppressUpdates = TRUE)
+
+
+gh.username <- "username"
+gh.password <- "password"
+
+install_github("bingd", "aaronwolen", 
+               auth_user = gh.username, 
+               password = gh.password)
+
+# data url: http://bit.ly/bingd-data
